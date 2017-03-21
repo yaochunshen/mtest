f4d1fd600f5d00ba007daabc4dd889dc2f6a0240
yaochunshen <yaochunshen@zbj.com>
2017-03-21 10:37:46 +0800

    addtest2

--- src/main/java/com/test/mtest/Test.java
+++ src/main/java/com/test/mtest/Test.java
@@ -3,19 +3,17 @@ package com.test.mtest;
 public class Test {
 	
 	public static void main(String [] args){
-<<<<<<< HEAD
 //		int t =10;
 //		System.out.println(0x1 << 1);
 //		System.out.println(t & (0x1 << 1 ));
 //		System.out.println((t & (0x1 << 1 )) >> 1);
-=======
+
 		int t =10;
 		System.out.println(0x1 << 1);
 		System.out.println(t & (0x1 << 1 ));
 		System.out.println((t & (0x1 << 1 )) >> 1);
 		System.out.println((t & (0x1 << 1 )) >> 1);
 		
->>>>>>> 319788a1d92a986ce900e9f80a371c8ecac22b57
 		
 		new B("f");
 	}
